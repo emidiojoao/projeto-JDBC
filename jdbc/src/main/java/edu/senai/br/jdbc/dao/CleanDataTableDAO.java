5e27d756cb3f36ee0d74062f7f32b36187ee07fa
emidiojoao <joaojoaocarlos045@gmail.com>
2024-11-04 19:19:09 -0300

    criando a tabela de limpeza

--- jdbc/src/main/java/edu/senai/br/jdbc/dao/CleanDataTableDAO.java
+++ jdbc/src/main/java/edu/senai/br/jdbc/dao/CleanDataTableDAO.java
@@ -11,36 +11,34 @@ import java.sql.SQLException;
  */
 public class CleanDataTableDAO {
 
-    public void deleteTabelaFilme() throws SQLException {
-        String sql = "DELETE FROM FILME;";
-        try (Connection conn = ConexaoDB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.executeUpdate();
-
-        }
-    }
 
     public void deleteTabelaCategoria() throws SQLException {
-        String sql = "DELETE FROM CATEGORIA;";
+        String sql = "DELETE FROM Categoria;";
         try (Connection conn = ConexaoDB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.executeUpdate();
-
         }
     }
 
-    public void setAutoIncrementeOneTabelaFilme() throws SQLException {
-        String sql = "ALTER TABLE FILME AUTO_INCREMENT=1;;";
-        try (Connection conn = ConexaoDB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.executeUpdate();
 
-        }
-    }
 
     public void setAutoIncrementeOneTabelaCategoria() throws SQLException {
-        String sql = "ALTER TABLE CATEGORIA AUTO_INCREMENT=1;;";
+        String sql = "ALTER TABLE Categoria AUTO_INCREMENT=1;";
         try (Connection conn = ConexaoDB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.executeUpdate();
-
         }
     }
-
+    
+    public void deleteTabelaFilme() throws SQLException {
+        String sql = "DELETE FROM Filme;";
+        try (Connection conn = ConexaoDB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.executeUpdate();
+        }
+    }
+    
+    public void setAutoIncrementOneTableFilme() throws SQLException {
+        String sql = "ALTER TABLE Categoria AUTO_INCREMENT=1;";
+        try (Connection conn = ConexaoDB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.executeUpdate();
+        }
+    }
 }
